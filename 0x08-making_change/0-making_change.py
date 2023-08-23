882afebe70a1c9ab1453fefcec83cf5ae0f7b50c
sofy <bedlusofi@gmail.com>
2023-08-23 18:42:02 +0300

    update-algo

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -27,10 +27,9 @@ def makeChange(coins, total):
 
     # Calculate the minimum number of coins needed for each total
     # value from 1 to 'total'
-    for i in range(1, total + 1):
-        for coin in coins:
-            if i - coin >= 0:
-                dp[i] = min(dp[i], dp[i - coin] + 1)
+    for coin in coins:
+        for i in range(coin, total + 1):
+            dp[i] = min(dp[i], dp[i - coin] + 1)
 
     # If the value at 'total' is still 'inf', it means the total cannot be met
     # by any combination of coins
